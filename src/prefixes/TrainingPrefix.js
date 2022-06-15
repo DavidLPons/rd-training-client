cb50b8f08a374738b5e560d6d4ad7e82bc328d32
David Lopez <david.pons@simpleviewinc.com>
2022-06-15 16:59:22 +0100

    GROK-1666 Fix Trainingprefix constructors

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -1,5 +1,5 @@
-const Movies = require('./collections/Movies')
-const People = require('./collections/People')
+const { Movies } = require('./collections/Movies')
+const { People } = require('./collections/People')
 
 class TrainingPrefix {
     constructor({ graphUrl, graphServer }) {
@@ -8,7 +8,7 @@ class TrainingPrefix {
         this._graphServer = graphServer;
 
         this.movies = new Movies({ graphUrl, graphServer });
-        this.people = new people({ graphUrl, graphServer });
+        this.people = new People({ graphUrl, graphServer });
     }
 }
 
